8541cd7f06280c28e030a2b6cd64cbbbe66e8278
Rob Haynes <robhayne0420@gmail.com>
2021-04-02 11:51:34 +0200

    Added Q2

---
+++ UCT/row.py
@@ -0,0 +1,11 @@
+#Rob Haynes
+#02/04/2021
+
+num = int(eval(input("Enter the start number:\n")))
+
+for x in range(0,6):
+    if num < 10 and num > 0:
+        print(f" {num} ", end='')
+    else:
+        print(f"{num} ", end='')
+    num += 1
