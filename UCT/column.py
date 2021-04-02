7fa0161609bc40f3f997404c47c19a116c4c4f81
Rob Haynes <robhayne0420@gmail.com>
2021-04-02 13:47:21 +0200

    Changed if statement

--- UCT/column.py
+++ UCT/column.py
@@ -5,7 +5,7 @@ num = int(eval(input("Enter a number: \n")))
 display = num
 
 for x in range(num,num+41, 7):
-    if display < 10 and display > 0:
+    if display < 10 and display >= 0:
         print(f" {display}")
     else:
         print(f"{display}")
--- UCT/row.py
+++ UCT/row.py
@@ -4,7 +4,7 @@
 num = int(eval(input("Enter the start number:\n")))
 
 for x in range(0,6):
-    if num < 10 and num > 0:
+    if num < 10 and num >= 0:
         print(f" {num} ", end='')
     else:
         print(f"{num} ", end='')
