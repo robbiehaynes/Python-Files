9d34ae991cc1837c24f96073024dd7cfe5b5605b
robbiehaynes <robhaynes0420@gmail.com>
2021-04-08 12:50:40 +0200

    Incomplete File

---
+++ UCT/printmonth.py
@@ -0,0 +1,8 @@
+#Rob Haynes
+#8 April 2021
+
+month = input("Enter the month('January', ...,'December'):\n")
+day  = input("Enter the start day ('Monday', ..., 'Sunday'):\n")
+numdays = 0
+
+thirty_one_set = {"January","March","May","July","August","October",""}
